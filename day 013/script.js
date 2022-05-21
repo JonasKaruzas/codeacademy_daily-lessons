5cdde5489309dd8469d8fa857d3b9ebc31ff620f
JonasKaruzas <EnterGitHub1>
2022-05-21 13:54:42 +0300

    CA Online lessons, Reponsive exercise

--- day 013/script.js
+++ day 013/script.js
@@ -1,1 +1,20 @@
-console.log('hi');
+function sum(a, b) {
+    return a+b
+}
+
+console.log(sum(1,5));
+
+const namai = {
+    zidinys: false,
+    zidynysUzkurtas: false,
+    uzkurtiZidini: function() {
+        if (this.zidynysUzkurtas) {
+            console.log(`Jau uzkurtas`);
+        } else {
+            this.zidynysUzkurtas = true;
+            console.log(`Uzkurkuriau`);
+        }
+    }
+}
+
+console.log(namai.zidinys);
